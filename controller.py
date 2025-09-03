53f73bad35a6bcb93720116d56e3290fd443b7e9
Althea Faye S. Uy <uy.althea.faye@gmail.com>
2025-09-03 23:56:21 +0800

    Update controller.py

--- controller.py
+++ controller.py
@@ -1,44 +1,31 @@
-from model import HangmanModel
+
+from model import HangmanModel, Verdict
 
 
 class HangmanController:
-    def __init__(self, word: str, lives: int) -> None:
-        self.model = HangmanModel(word, lives)
+    def __init__(self, answer: str, lives: int) -> None:
+        self._model = HangmanModel(answer, lives)
 
-    def get_current_progress(self) -> str:
-        return self.model.get_current_progress()
+    def guess(self, char: str) -> Verdict:
+        return self._model.make_guess(char)
 
-    def get_all_guesses(self) -> list[str]:
-        return self.model.get_all_guesses()
+    def progress(self) -> str:
+        return self._model.get_current_progress()
 
-    def get_lives_left(self) -> int:
-        return self.model.lives_left
+    def guesses(self) -> list[str]:
+        return self._model.get_all_guesses()
 
-    def get_answer(self) -> str | None:
-        return self.model.answer
+    def lives(self) -> int:
+        return self._model.lives_left
 
-    def get_answer_for_display(self) -> str:
-        return self.model.get_answer_for_display()
+    def is_done(self) -> bool:
+        return self._model.is_game_done
 
-    def has_already_guessed(self, guess: str) -> bool:
-        return self.model.has_already_guessed(guess)
+    def did_win(self) -> bool:
+        return self._model.did_player_win
 
-    def is_valid_guess(self, guess: str) -> bool:
-        return self.model.is_valid_guess(guess)
+    def did_lose(self) -> bool:
+        return self._model.did_player_lose
 
-    def make_guess(self, guess: str) -> None:
-        if self.model.did_player_win or self.model.did_player_lose:
-            return  # Ignore guesses after game ends
-        if not self.is_valid_guess(guess):
-            return  # Ignore invalid guess
-        if self.has_already_guessed(guess):
-            return  # Ignore duplicate guess
-        self.model.make_guess(guess)
-
-    def player_won(self) -> bool:
-        return self.model.did_player_win
-
-    def player_lost(self) -> bool:
-        return self.model.did_player_lose
-
-
+    def final_answer(self) -> str | None:
+        return self._model.answer
