b45c9a8172d5a87c37c4a88f51a02f04cf8f287e
Althea Faye S. Uy <uy.althea.faye@gmail.com>
2025-09-03 23:15:54 +0800

    Update model.py

--- model.py
+++ model.py
@@ -6,26 +6,26 @@ class HangmanModel:
         if lives <= 0:
             raise ValueError("lives must be at least 1")
 
-        self._answer = answer.lower()
+        self._answer: str = answer.lower()
         
-        self._starting_lives = lives
-        self._lives_left = lives
+        self._starting_lives: int = lives
+        self._lives_left:int = lives
 
         self._correct_guesses: set[str] = set()
         self._incorrect_guesses: set[str] = set()
 
-        self._game_over = False
-        self._player_winner = False
+        self._game_over: bool = False
+        self._player_winner: bool = False
 
 
 
-    def make_guess(self, guess: str):
+    def make_guess(self, guess: str) -> None:
         if len(guess) != 1:
             return 
         if self._game_over == True:
             return
 
-        guess_lower = guess.lower()
+        guess_lower: str = guess.lower()
 
         if guess_lower in self._correct_guesses or guess_lower in self._incorrect_guesses:
             return
@@ -39,7 +39,7 @@ class HangmanModel:
         self.check_game_state()
 
     def check_game_state(self) -> None:
-        guessed_the_answer = set(self._answer)
+        guessed_the_answer: set[str] = set(self._answer)
         if guessed_the_answer <= self._correct_guesses and self._lives_left > 0:
             self._game_over = True
             self._player_winner = True
@@ -49,7 +49,7 @@ class HangmanModel:
 
     @property
     def answer(self) -> str | None:
-        return self._answer if self._game_over == True else None
+        return self._answer if self._game_over else None
 
     @property
     def lives_left(self) -> int:
@@ -57,18 +57,18 @@ class HangmanModel:
 
     @property
     def did_player_win(self) -> bool:
-        return True if self._game_over and self._player_winner else False
+        return True if self._game_over and self._player_winner
 
     @property
     def did_player_lose(self) -> bool:
-        return True if self._game_over and not self._player_winner else False
+        return True if self._game_over and not self._player_winner
 
     def get_all_guesses(self) -> list[str]:
-        all_guesses = self._correct_guesses | self._incorrect_guesses
+        all_guesses: set[str] = self._correct_guesses | self._incorrect_guesses
         return sorted(all_guesses)
 
     def guess_is_valid(self, guess: str) -> bool:
-        return True if len(guess) == 1 else False
+        return len(guess) == 1
 
     def guess_is_already_guessed(self, guess: str) -> bool:
         if len(guess) != 1:
@@ -76,5 +76,5 @@ class HangmanModel:
         return guess.lower() in self._incorrect_guesses or guess.lower() in self._correct_guesses
 
     def display_answer(self) -> str:
+        return " ".join(char if char in self._correct guesses else "_" for c in self._answer)
 
-        return self._answer
